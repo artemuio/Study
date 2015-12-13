85002d1a84fc646c6a58a30615befba155088017
mr.Horlos <artem-yovenko@yandex.ru>
2015-12-13 15:43:55 +0200

    Routing

--- 2napp-web/routes/index.js
+++ 2napp-web/routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var fs = require("fs");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+  start(req,res);
 });
+
+function start(req,res) {
+  if (!req.isAuthenticated()) {
+    fs.readFile('index.html', function (err, info) {
+      if (err) {
+        console.log("Error in reading index.html");
+        res.writeHead(500);
+        res.end("Error in server");
+        return;
+      }
+      res.end(info);
+    });
+  } else {
+    res.redirect('/profile');
+  }
+}
 
 module.exports = router;
--- 2napp-web/routes/profile.js
+++ 2napp-web/routes/profile.js
@@ -1,9 +1,12 @@
+/**
+ * Created by artem on 13.12.2015.
+ */
 var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+    res.send('respond with a resource');
 });
 
 module.exports = router;
